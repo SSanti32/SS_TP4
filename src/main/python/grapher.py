b0db1c4a2823e8e2e3a234922a62429f0846977e
slarroude <slarroude@itba.edu.ar>
2023-05-09 19:28:56 -0300

    FIX: grapher.py labels

--- src/main/python/grapher.py
+++ src/main/python/grapher.py
@@ -7,20 +7,20 @@ file3 = open("../resources/gearAlg.txt", "r")
 file4 = open("../resources/analyticalVerlet.txt", "r")
 
 # Leer archivos y almacenar datos
-x1 = [float(line) for line in file1]
-x2 = [float(line) for line in file2]
-x3 = [float(line) for line in file3]
-x4 = [float(line) for line in file4]
+verletAlg = [float(line) for line in file1]
+beemanAlg = [float(line) for line in file2]
+gearAlg = [float(line) for line in file3]
+analytical = [float(line) for line in file4]
 
 # Obtener valores máximos y mínimos de cada función
-x1_max = max(x1)
-x1_min = min(x1)
-x2_max = max(x2)
-x2_min = min(x2)
-x3_max = max(x3)
-x3_min = min(x3)
-x4_max = max(x4)
-x4_min = min(x4)
+verletAlg_max = max(verletAlg)
+verletAlg_min = min(verletAlg)
+beemanAlg_max = max(beemanAlg)
+beemanAlg_min = min(beemanAlg)
+gearAlg_max = max(gearAlg)
+gearAlg_min = min(gearAlg)
+analytical_max = max(analytical)
+analytical_min = min(analytical)
 
 # Cerrar archivos
 file1.close()
@@ -29,17 +29,17 @@ file3.close()
 file4.close()
 
 # Crear variable t
-t = list(range(len(x1)))
-t_2 = list(range(len(x2)))
-t_3 = list(range(len(x3)))
-t_4 = list(range(len(x4)))
+t = list(range(len(verletAlg)))
+t_2 = list(range(len(beemanAlg)))
+t_3 = list(range(len(gearAlg)))
+t_4 = list(range(len(analytical)))
 
 # Graficar funciones
-plt.semilogy(t, x1, label="Función 1")
-plt.semilogy(t_2, x2, label="Función 2")
-plt.semilogy(t_3, x3, label="Función 3")
-plt.semilogy(t_4, x4, label="Función 4")
-# plt.ylim([min(x1_min, x2_min, x3_min, x4_min), max(x1_max, x2_max, x3_max, x4_max)])
+plt.semilogy(t, verletAlg, label="verletAlg")
+plt.semilogy(t_2, beemanAlg, label="beemanAlg")
+plt.semilogy(t_3, gearAlg, label="gearAlg")
+plt.semilogy(t_4, analytical, label="analytical")
+# plt.ylim([min(verletAlg_min, beemanAlg_min, gearAlg_min, analytical_min), max(verletAlg_max, beemanAlg_max, gearAlg_max, analytical_max)])
 
 # Agregar leyenda y etiquetas
 plt.legend()
